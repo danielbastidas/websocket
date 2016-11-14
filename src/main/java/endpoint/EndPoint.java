5579b3e1b8344850bfcb49224c2db9eacb5c22f8
daniel.bastidas <daniel.bastidas@smartmatic.com>
2016-11-14 18:26:07 -0500

    adding timer and random support

--- src/main/java/endpoint/EndPoint.java
+++ src/main/java/endpoint/EndPoint.java
@@ -1,7 +1,12 @@
 package endpoint;
 
 import java.io.IOException;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
+import javax.annotation.Resource;
 import javax.ejb.Stateless;
 import javax.websocket.CloseReason;
 import javax.websocket.OnClose;
@@ -17,6 +22,9 @@ import org.jboss.logging.Logger;
 public class EndPoint {
 	Logger log = Logger.getLogger(this.getClass());
 	
+	ExecutorService executor = Executors.newSingleThreadExecutor();
+	Random rand = new Random();
+	
 	@OnMessage
 	public String receiveMessage(String message, Session session) {
 		log.info("Received : "+ message + ", session:" + session.getId());
@@ -24,9 +32,29 @@ public class EndPoint {
 	}
 	
 	@OnOpen
-	public void open(Session session) throws IOException {
+	public void open(final Session session) throws IOException {
 		log.info("Open session:" + session.getId());
-		session.getBasicRemote().sendText("Message from server");
+		
+		executor.execute(new Runnable() {
+			public void run() {				
+				try {
+					int candidate1 = 0;
+					int candidate2 = 0;
+					while(true) {
+					 Thread.sleep(5000);
+					 
+					 candidate1 = rand.nextInt(101); 
+					 candidate2 = rand.nextInt(101);
+					 
+					 session.getBasicRemote().sendText(candidate1 + " " + candidate2);
+					} 
+				} catch (InterruptedException | IOException e) {
+					e.printStackTrace();
+				}
+			}			
+		});
+		
+//		session.getBasicRemote().sendText("Message from server");
 		
 	}
 	
